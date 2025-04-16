d04c3efd927a054948efd04f92d9fea858ca1c95
mulan <kbecerra774@gmail.com>
2025-04-16 14:24:19 -0600

    Agregar archivos de proyecto.

---
+++ BasculaInterface/MainPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.Drawing;
+using System.Drawing.Printing;
+using System.IO.Ports;
+using System.Timers;
+
+namespace BasculaInterface
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly SerialPort _bascula;
+
+        private System.Timers.Timer _pollingTimer;
+
+        public double Tara { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _bascula = new SerialPort(MauiProgram.PortName, MauiProgram.PortBaud, Parity.None, 8, StopBits.One);
+
+            _bascula.DataReceived += new SerialDataReceivedEventHandler(DataRecievedHandler);
+
+            if (MauiProgram.NeedManualAsk)
+            {
+                _pollingTimer = new(1000);
+                _pollingTimer.Elapsed += new ElapsedEventHandler(OnTimerElapsed);
+                _pollingTimer.AutoReset = true;
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _bascula.Open();
+
+            if (MauiProgram.NeedManualAsk)
+                _pollingTimer.Start();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _bascula.Close();
+
+            if (MauiProgram.NeedManualAsk)
+                _pollingTimer.Stop();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                if (_bascula.IsOpen)
+                {
+                    // Este comando depende de tu modelo de báscula.
+                    // Muchos usan "P\r\n" o solo "\r\n" para pedir el peso.
+                    _bascula.Write(MauiProgram.AskChar);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al enviar comando: {ex.Message}");
+            }
+        }
+
+        private void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string readData = sp.ReadExisting();
+
+
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                if (Tara != 0)
+                {
+                    double currentWeight = ParseScreenWeight(readData);
+
+                    PesoLabel.Text = $"{currentWeight - Tara} kg";
+                }
+                else
+                {
+                    PesoLabel.Text = readData;
+                }
+            });
+        }
+
+        private void OnTaraClicked(object sender, EventArgs e)
+        {
+            Tara = ParseScreenWeight(PesoLabel.Text);
+
+            TaraLabel.Text = Tara.ToString("0.000 kg");
+        }
+
+        private double ParseScreenWeight(string value)
+        {
+            string clearedString = value.Replace("kg", "", StringComparison.OrdinalIgnoreCase).Trim();
+
+            if (double.TryParse(clearedString, System.Globalization.NumberStyles.Any,
+                        System.Globalization.CultureInfo.InvariantCulture, out double peso))
+            {
+                return peso;
+            }
+
+            return 0;
+        }
+
+        private void OnImprimirClicked(object sender, EventArgs e)
+        {
+#if WINDOWS
+            PrintDocument document = new();
+            document.PrinterSettings.PrinterName = MauiProgram.PrinterName;
+
+            document.PrintPage += (sender, e) =>
+            {
+                System.Drawing.Font font = new ("Courier New", 10);
+                e.Graphics.DrawString($"COOPERATIVA PEDRO EZQUEDA\nTara: {Tara}\nPeso: {PesoLabel.Text}\nDiferencia: {ParseScreenWeight(PesoLabel.Text)}", font, Brushes.Black, 10, 10);
+            };
+
+            document.Print();
+#endif
+        }
+
+        private void OnCeroClicked(object sender, EventArgs e)
+        {
+            Tara = 0;
+            TaraLabel.Text = "0.000 kg";
+        }
+    }
+
+}
---
+++ BasculaInterface/MauiProgram.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace BasculaInterface;
+
+public static class MauiProgram
+{
+    public static string PortName = null!;
+    public static int PortBaud = 9600;
+    public static string PrinterName = null!;
+    public static bool NeedManualAsk = false;
+    public static string AskChar = "P";
+    public static MauiApp CreateMauiApp()
+	{
+        LoadSettings();
+
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+
+    private static void LoadSettings()
+    {
+        string settingsFile = Path.Combine(AppContext.BaseDirectory, "Config/settings.json");
+        if (!File.Exists(settingsFile))
+        {
+            throw new InvalidDataException("El archivo settings.json no existe");
+        }
+
+        string jsonContent = File.ReadAllText(settingsFile);
+
+        var data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(jsonContent);
+
+        if (data is null)
+            throw new InvalidDataException("El archivo settings.json parece ser nulo");
+
+        PortName = data["BasculaPort"].GetString();
+        PortBaud = data["BasculaBauds"].GetInt32();
+        if (PortBaud == 0)
+            PortBaud = 9600;
+        PrinterName = data["PrinterName"].GetString();
+        NeedManualAsk = data["NeedManualAsk"].GetBoolean();
+        if (NeedManualAsk)
+            AskChar = data["AskChar"].GetString();
+    }
+}
