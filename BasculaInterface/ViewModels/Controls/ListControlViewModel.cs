aa665ca3b596f2d7a38bdef8b3aa30a29015eaab
Diego Alfredo Soto Jaquez <diego.soto@venteks.com>
2025-10-18 16:24:45 -0600

    se hicieron cambios en la ui

---
+++ BasculaInterface/ViewModels/Controls/ListControlViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Core.Domain.Entities.ContpaqiSQL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasculaInterface.ViewModels.Controls
+{
+    public class ListControlViewModel<T>
+    {
+        public ObservableCollection<T> BindingList { get; set; }
+
+        public ListControlViewModel(ObservableCollection<T> bindingList)
+        {
+            BindingList = bindingList;
+        }
+    }
+}
